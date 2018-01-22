5396a88a573cf941c7aa9e01fdbbe52c901b3e7e
didier <didier@d-defrance.fr>
2018-01-22 23:40:02 +0100

    nouvelle version

--- init_push.bat
+++ init_push.bat
@@ -3,7 +3,7 @@ git init
 git add *
 git commit -a -m "version initiale"
 git remote remove gitHubOriginCordovaPlugin
-git remote add gitHubOriginCordovaPlugin https://didier-mycontrib:gh14d2f69sm27x@github.com/didier-mycontrib/cordova_plugin.git
+git remote add gitHubOriginCordovaPlugin https://didier-mycontrib:gh14.....sm..x@github.com/didier-mycontrib/cordova_plugin.git
 git push -u gitHubOriginCordovaPlugin master
 pause
 
